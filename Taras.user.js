43281483e2f751f91227683512431fa61ea43fc7
DmytroPEAR <160066593+DmytroPEAR@users.noreply.github.com>
2026-04-15 15:59:56 +0300

    Update Taras.user.js

--- Taras.user.js
+++ Taras.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Taras
 // @namespace    local
-// @version      1.0
+// @version      1.1
 // @description  Auto video + doc
 // @match        https://talent.shixizhi.huawei.com/*
 
